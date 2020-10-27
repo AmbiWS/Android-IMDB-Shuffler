7479830cae9da9ded365ac732b6396ef711c77b9
Ambi WS Studio <ambi.ws.studio@gmail.com>
2020-10-27 18:37:54 +0200

    Project added to VCS.

---
+++ app/src/main/java/com/ambiwsstudio/aliexpress_shuffler/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ambiwsstudio.aliexpress_shuffler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

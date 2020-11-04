4f995a6a50f36a84e518e9ee78438eb5c0141a18
Ambi WS Studio <ambi.ws.studio@gmail.com>
2020-11-04 05:56:47 +0200

    Added WatchLater and CheckList static buttons over MovieFragment

--- app/src/main/java/com/ambiwsstudio/movie_shuffler/viewmodel/MovieCollectionViewModel.java
+++ app/src/main/java/com/ambiwsstudio/movie_shuffler/viewmodel/MovieCollectionViewModel.java
@@ -1,9 +1,62 @@
 package com.ambiwsstudio.movie_shuffler.viewmodel;
 
-import androidx.lifecycle.ViewModel;
+import android.app.Application;
+import android.graphics.drawable.ColorDrawable;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
-public class MovieCollectionViewModel extends ViewModel {
+import com.ambiwsstudio.movie_shuffler.R;
 
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.MutableLiveData;
 
+public class MovieCollectionViewModel extends AndroidViewModel {
+
+    private MutableLiveData<Boolean> isMovieToWatch;
+    private MutableLiveData<Boolean> isAccessedToList;
+
+    public MovieCollectionViewModel(@NonNull Application application) {
+        super(application);
+    }
+
+    public MutableLiveData<Boolean> getIsMovieToWatch() {
+
+        if (isMovieToWatch == null)
+            isMovieToWatch = new MutableLiveData<>();
+
+        return isMovieToWatch;
+
+    }
+
+    public MutableLiveData<Boolean> getIsAccessedToList() {
+
+        if (isAccessedToList == null)
+            isAccessedToList = new MutableLiveData<>();
+
+        return isAccessedToList;
+
+    }
+
+    public void onListClick(View view) {
+
+        Log.i("MovieCollectionVM", "ListClick Caught: " + view.toString());
+        isAccessedToList.setValue(true);
+
+    }
+
+    public void onSaveClick(View view) {
+
+        Log.i("MovieCollectionVM", "SaveClick Caught: " + view.toString());
+        ImageView imageView = (ImageView)view;
+        if (((ColorDrawable)imageView.getBackground()).getColor()
+                == ((ColorDrawable)getApplication().getResources().getDrawable(R.color.colorLightTrans)).getColor()) {
+
+            isMovieToWatch.setValue(true);
+
+        } else isMovieToWatch.setValue(false);
+
+    }
 
 }
